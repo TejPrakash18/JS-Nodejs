a5cc28fb34dca5184925b81f02c5ac18134d1c46
Tejprakash18 <tej22upa.dhyay@gmail.com>
2024-12-04 21:23:20 +0530

    JavaScript

---
+++ js/Hello.js
@@ -0,0 +1,17 @@
+let userName = "Tej Upadhyay"
+var age = 23
+const email = "tej22upa.dhyay@gmail.com"
+
+console.log('"type of operator" : ', typeof(age))
+
+console.log(`"name": ${userName}`,`"age": ${age}`,`"email": ${email}`)
+console.log("new line : ",'\n')
+console.log(`Hello I,m "Tej Upadhyay" `);
+
+//type conversion
+let number = 12
+let user = "Tej"
+
+console.log("number + string",number+user)
+console.log("number * string",number*user)
+console.log("string + string",user+user)
