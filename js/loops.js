ff0b14f13350c4fd57dad0e99da53dfdfc8a96ed
Tejprakash18 <tej22upa.dhyay@gmail.com>
2024-12-05 07:47:34 +0530

    Loops

--- js/loops.js
+++ js/loops.js
@@ -3,3 +3,12 @@ for (let number = 1; number <= 10; number++) {
     console.log(number);
     
 }
+let person = {fname: "Tej", lname:"Upadhyay", age:23}
+for (let key in person) {
+    console.log(person[key])
+}
+
+const cars = ["Mahindra BE 6e", "BMW", "Honda"];
+for (const element of cars) {
+    console.log(element)
+}
