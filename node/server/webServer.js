31945ebe7e39aebb3dda8e5c2c580b161dab4035
Tejprakash18 <tej22upa.dhyay@gmail.com>
2024-12-08 07:35:37 +0530

    http server

--- node/server/webServer.js
+++ node/server/webServer.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const fs = require("fs");
+const { isUtf8 } = require("buffer");
 
 const myServer = http.createServer((req, res) => {
-    console.log(req.headers)
-    res.end("server")
+    res.header = 200;
+    const log = `${Date.now()}: new req is coming + " " +  ${res.header}\n`;
+    fs.appendFile('log.txt', log, (err, data)=>{
+        console.log(req.headers)
+        res.end("Hello Server!")
+    })
+    
 })
 
 myServer.listen(8080, ()=> console.log("server started !"))
