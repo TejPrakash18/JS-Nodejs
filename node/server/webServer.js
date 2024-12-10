c570f422a9491c42303ef8dc5e4b6cf1d46d93f5
Tejprakash18 <tej22upa.dhyay@gmail.com>
2024-12-10 07:44:33 +0530

    .env file

--- node/server/webServer.js
+++ node/server/webServer.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const http = require("http");
 const fs = require("fs");
 
@@ -6,10 +8,15 @@ const myServer = http.createServer((req, res) => {
     res.header = 200;
     const log = `${Date.now()}: new req is coming + " " +  ${res.header}\n`;
     fs.appendFile('log.txt', log, (err, data)=>{
-        console.log(req.headers)
+        console.log("this is the node project");
+        console.log("this is new line");
+
+        console.log(process.env.NAME);
+        console.log(process.env.PROFESSION);
+        console.log("I am starting a new series on ",process.env.COURSE);
         res.end("Hello Server!")
     })
     
 })
 
-myServer.listen(8080, ()=> console.log("server started !"))
+myServer.listen(8080, ()=> console.log("server started !"));
