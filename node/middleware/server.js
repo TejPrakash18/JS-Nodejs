29a6b6b502b9903902ad636191ab679c6e1f5c69
Tejprakash18 <tej22upa.dhyay@gmail.com>
2024-12-13 19:09:49 +0530

    middleware

--- node/middleware/server.js
+++ node/middleware/server.js
@@ -1,9 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req ,res) =>{
-    logRequest(req, res);
+    res.statusCode = 200;
 
+    switch(req.method){
+        case "GET":
+            getReq(req,res);
+            break;
+        case "POST":
+            postReq(req,res);
+            break;
+    }
+});
 
-
-    
+server.addListener(5001, '127.0.0.1' ,()=>{
+    console.log(`server at running at the point http://127.0.0.1`)
 })
