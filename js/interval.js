c8d32f445d66f24781cba098fc64a471c64d4999
Tejprakash18 <tej22upa.dhyay@gmail.com>
2024-12-31 11:36:15 +0530

    intervl

--- js/interval.js
+++ js/interval.js
@@ -1,6 +1,16 @@
+console.log("Interval..!")
+
+function hello(user){
+    console.log(`hello, ${user}`)
+}
+
+setTimeout(hello, 2000, "Tej");
+
 function greet(name) {
     console.log(`Hello, ${name}!`);
 }
 
-setInterval(greet, 3000, "Bob");
-// Output every 3 seconds: Hello, Bob!
+setInterval(greet, 3000, "Tej");
+// Output every 3 seconds: Hello, Tej!
+
+
