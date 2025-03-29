42c41629c868bf94c9d59369c4a16d54cf8c6c26
Tejprakash18 <tej22upa.dhyay@gmail.com>
2025-03-29 22:57:46 +0530

    DOM

--- js/errorHandling.js
+++ js/errorHandling.js
@@ -1,26 +1,42 @@
-try {
-    dadAlert("Welcoe.....!")
-} catch (error) {
-    console.log(error);
-}
+// try {
+//     dadAlert("Welcoe.....!")
+// } catch (error) {
+//     console.log(error);
+// }
 
-function geekFunc() {
-    let a = 10;
+// function geekFunc() {
+//     let a = 10;
+//     try {
+//         console.log("Value of variable a is : " + a);
+//     }
+//     catch (e) {
+//         console.log("Error: " + e.description);
+//     }
+// }
+// geekFunc();
+
+// try {
+//     throw new Error("ohhh oops...!")
+// } catch (e) {
+//     console.log(e);
+// }
+// finally{
+//     console.log("finally block is always execute")
+// }
+// console.log("code successfully run");
+
+const divide = (a, b) => {
     try {
-        console.log("Value of variable a is : " + a);
+        if (b === 0) {
+            throw new Error("Cannot divide by zero");
+        }
+        let result = a / b;
+        return result;
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        console.log("Division operation attempted.");
     }
-    catch (e) {
-        console.log("Error: " + e.description);
-    }
-}
-geekFunc();
+};
 
-try {
-    throw new Error("ohhh oops...!")
-} catch (e) {
-    console.log(e);
-}
-finally{
-    console.log("finally block is always execute")
-}
-console.log("code successfully run");
+console.log(divide(5,0))
--- js/interval.js
+++ js/interval.js
@@ -10,7 +10,17 @@ function greet(name) {
     console.log(`Hello, ${name}!`);
 }
 
-setInterval(greet, 3000, "Tej");
+setInterval(greet, 3000, "Tej", clearInterval());
 // Output every 3 seconds: Hello, Tej!
 
 
+let timer = setInterval(() => console.log("Running..."), 2000);
+setTimeout(() => clearInterval(timer), 7000); // Stops after 7 seconds
+
+
+let count = 0;
+let intervalID = setInterval(() => {
+    console.log(`Count: ${count}`);
+    count++;
+    if (count === 5) clearInterval(intervalID); // Stop after 5 iterations
+}, 1000);
